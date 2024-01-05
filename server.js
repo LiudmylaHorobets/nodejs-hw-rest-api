c5d8259efc7eeb26e88b550c3d6f562366d566a6
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-05 15:56:07 +0200

    #3

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require("./app");
 const mongoose = require("mongoose");
+const app = require("./app");
 
-const { DB_HOST, PORT = 3000 } =
+const DB_HOST =
   "mongodb+srv://user_goit:10y4rlyn4XqJy2uu@cluster0.hb4g0oz.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
@@ -9,7 +9,7 @@ mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(PORT, () => console.log("Database connection successful"));
+    app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
