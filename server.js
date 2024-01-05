3e58caa2b80d68e65d4be5f77b9bc6cf9d8fcca6
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-05 16:44:48 +0200

    #4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const app = require("./app");
 
 const DB_HOST =
-  "mongodb+srv://user_goit:10y4rlyn4XqJy2uu@cluster0.hb4g0oz.mongodb.net/contacts_reader?retryWrites=true&w=majority";
+  "mongodb+srv://liudmyla:PQylIjN40H8EsCRP@cluster1.lksphz6.mongodb.net/contacts_reader?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 
@@ -19,3 +19,5 @@ mongoose
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
 // });
+
+// PQylIjN40H8EsCRP
