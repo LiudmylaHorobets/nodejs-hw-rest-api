9a4266dcb906e7904cc8bacc59c37f4a5a5ba3ff
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-05 22:13:48 +0200

    #5

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 const app = require("./app");
 
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.set("strictQuery", true);
 
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT, () => console.log("Database connection successful"));
   })
   .catch((error) => {
     console.log(error.message);
@@ -19,4 +19,4 @@ mongoose
 //   console.log("Server running. Use our API on port: 3000");
 // });
 
-// PQylIjN40H8EsCRP
+// user b8pa8PaSStE7Akxo
