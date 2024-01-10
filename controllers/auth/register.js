bc01f6e061a30306d39a4ade782f68cdcff759f4
LiudmylaHorobets <horobets1323@gmail.com>
2024-01-10 21:31:08 +0200

    finished login

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,20 @@
 const { User } = require("../../models/user");
 
-const { ctrlWrapper } = require("../../helpers");
+const bcrypt = require("bcrypt");
+
+const { ctrlWrapper, HttpError } = require("../../helpers");
 
 const register = async (req, res) => {
-  const newUser = await User.create({ ...req.body });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email already in use");
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await User.create({ ...req.body, password: hashPassword });
 
   res.status(201).json({
     name: newUser.name,
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,6 +8,9 @@ const ctrl = require("../../controllers/auth");
 
 const router = express.Router();
 
+// signup
 router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+// signin
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 module.exports = router;
